a46b7cb8f0bbf185384885734c05071432b1c6cc
iddgithub <1584431845@qq.com>
2019-01-11 10:42:59 +0800

    init

---
+++ Desktop/Vue课间/day Vue06/hello-world/src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// Vue.config.productionTip = false
+import './assets/statics/site/css/style.css'
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
